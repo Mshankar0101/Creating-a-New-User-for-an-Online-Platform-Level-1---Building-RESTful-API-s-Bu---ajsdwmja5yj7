63433ce1cf75560f214cdcf01359377e86ac4945
Mshankar0101 <144152894+Mshankar0101@users.noreply.github.com>
2024-03-13 16:29:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,39 +12,34 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 // Write POST endpoint for registering new user
-// app.post("/api/v1/details",(req,res)=>{
-//    const {name, mail, number} = req.body;
-//    const id = userDetails.length;
-//    if(name &&  mail  && number){
-//       const newProduct = {
-//          "id": parseInt(id)+ 1,
-//          "name": name,
-//          "mail": mail,
-//          "number": number
-//       }
-//       userDetails.push(newProduct);
-//       fs.writeFileSync(`${__dirname}/data/userDetails.json`,JSON.stringify(userDetails),(error)=>{
-//         console.log("error in write file",error);
-//       });
-//      return res.status(201).send({"status": "Success","message": "User registered successfully", "data": newProduct});
-//    }
-//    return res.status(400).send({"error" :"please enter name, mail and number feild correctly"});
-
-// })
-
 app.post("/api/v1/details", (req, res) => {
-  const { name, mail, number } = req.body;
-  if (!name || !mail || !number) {
-  return res.status(400).send({"error": "Please enter name, mail, and number fields correctly"});
-  }
-  
-  const newId = userDetails.length>0? userDetails[userDetails.length - 1].id + 1 : 1; 
-  // const newProduct = { id: newId, name, mail, number };
-  const newUser = { id: newId, name, mail, number };
-  userDetails.push(newUser);
-  fs.writeFileSync(`${__dirname}/data/userDetails.json`, JSON.stringify(userDetails, null, 2));
-  return res.status(201).send({status : "Success", message: "User registered successfully", data: {newProduct : newUser}});
-  });
+    const { name, mail, number } = req.body;
+    const id = userDetails.length;
+    if (name && mail && number) {
+        const newProduct = {
+            "id": parseInt(id) + 1,
+            "name": name,
+            "mail": mail,
+            "number": number
+        };
+        userDetails.push(newProduct);
+        try {
+            fs.writeFileSync(`${__dirname}/data/userDetails.json`, JSON.stringify(userDetails, null, 2));
+            return res.status(201).send({
+                "status": "Success",
+                "message": "User registered successfully",
+                "data": { newProduct }
+            });
+        } catch (error) {
+            console.error("Error writing to file", error);
+         return res.status(500).send({ "error": "An error occurred while saving user data." });
+        }
+    } else {
+        return res.status(400).send({ "error": "Please enter name, mail, and number fields correctly" });
+    }
+});
+
+
 
 // GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
